701069c91ecb6c7f5e624fb286c657628e1d5045
batzorig <batzorig.enkh@gmail.com>
2019-11-17 20:36:47 +0800

    add components of header footer home and category

---
+++ resources/js/components/category/Index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Add from "./Add";
+import Listing from "./Listing";
+
+export default class Index extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <div>
+                        <Link to="/category">Listing</Link>
+                        <Link to="/category/Add">Add</Link>
+                        <Route exact path="/category" component={Add}/>
+                        <Route exact path="/category/add" component={Listing}/>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
+}
+
---
+++ resources/js/components/category/Listing.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Listing extends Component {
+    render() {
+        return (
+            <div>
+                this is Listing
+            </div>
+        );
+    }
+}
+
