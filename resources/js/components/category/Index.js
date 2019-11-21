42ca0d5282a195ba1c6b4c565da094940619a734
batzorig <batzorig.enkh@gmail.com>
2019-11-22 00:18:56 +0800

    update warn

--- resources/js/components/category/Index.js
+++ resources/js/components/category/Index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Add from "./Add";
 import Listing from "./Listing";
+import Edit from "./Edit";
 
 export default class Index extends Component {
     render() {
@@ -12,8 +13,9 @@ export default class Index extends Component {
                         <hr />
                         <Link className="btn btn-primary" to="/category">Listing</Link>&nbsp;
                         <Link className="btn btn-primary" to="/category/Add">Add</Link>
-                        <Route exact path="/category" component={Listing}/>
-                        <Route exact path="/category/add" component={Add}/>
+                        <Route exact path="/category" component={Listing} />
+                        <Route exact path="/category/add" component={Add} />
+                        <Route exact path="/category/edit/::id" component={Edit} />
                         <hr />
                     </div>
                 </Router>
